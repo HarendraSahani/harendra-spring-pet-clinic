99cf200bcb21a8463686f51c73d75d84320b9314
Harendra <koolzharry@gmail.com>
2021-11-30 00:35:11 -0600

    first commit from my perosnal machine

---
+++ src/test/java/com/spring/learn4free/harendra/HarendraSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learn4free.harendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarendraSpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
